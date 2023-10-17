aa884f413516d63e438fc0aea68279e474de3831
akshayp2 <akshaypatil19971710@gmail.com>
2023-10-17 22:54:54 +0530

    created pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import { useState } from 'react'
 
 import './App.css'
+import { BrowserRouter, Routes,Route } from 'react-router-dom'
+import SingIn from './Pages/SingIn'
+import SingUp from './Pages/SingUp'
+import About from './Pages/About'
+import Home from './Pages/Home'
+import Profile from './Pages/Profile'
 
 function App() {
  
 
   return (
-    <>
-    <h1 className=''>how are you</h1>
-    </>
+    <BrowserRouter>
+    <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/sing-in' element={<SingIn/>}/>
+    <Route path='/sing-up' element={<SingUp/>}/>
+    <Route path='/about' element={<About/>}/>
+    <Route path='/profile' element={<Profile/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
