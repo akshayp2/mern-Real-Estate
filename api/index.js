cc447f43016ef776aaa962fae10fedfc53f8d939
akshayp2 <akshaypatil19971710@gmail.com>
2023-10-20 16:40:19 +0530

    created middleware and function to check possible error

--- api/index.js
+++ api/index.js
@@ -4,20 +4,14 @@ import { connect } from 'mongoose';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 
-configDotenv({ path: '../.env' }); 
+configDotenv({ path: '../.env' });
 
-connect(process.env.MONGO)
-  .then(() => {
+connect(process.env.MONGO).then(() => {
     console.log('Connected to MongoDB!');
-  })
-  .catch(err => console.log(err));
+}).catch(err => console.log(err));
 
 const app = express();
 app.use(express.json())
-
-
-
-
 
 const port = 3000;
 app.listen(port, () => {
@@ -26,5 +20,15 @@ app.listen(port, () => {
 
 
 
-app.use('/api/user',userRouter);
-app.use('/api/auth',authRouter);
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message
+  });
+})
