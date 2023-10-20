d355b4454245de041dd02ceb29eecec66e6ead85
akshayp2 <akshaypatil19971710@gmail.com>
2023-10-20 10:40:24 +0530

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
+const { config: configDotenv } = require('dotenv');
 const express = require('express');
+const mongoose = require('mongoose');
+
+configDotenv({ path: '../.env' }); 
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch(err => console.log(err));
 
 const app = express();
-
-app.listen(3000,()=>{
-    console.log('server is running on port 3000!!');
-})
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Connected to port ${port}`);
+});
